622b553b3430765c7fe5ce13d8e90ec92939aa6a
Dorabang <psa1195@naver.com>
2023-08-24 01:25:02 +0900

    feat: path가 있는 메뉴일 경우 클릭 시 해당 경로로 이동, 모달창이 뜨는 메뉴는 모달창 팝업

--- src/components/Menu/MenuButton.tsx
+++ src/components/Menu/MenuButton.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { IconType } from 'react-icons';
 import SelectIcon from './SelectIcon';
 import ProfileIcon from '../ProfileIcon';
+import { useNavigate } from 'react-router-dom';
+import { modalOpenState, navState } from '../../recoil/atoms';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 
 interface MenuButtonProps {
   icon: IconType;
@@ -26,6 +29,23 @@ const MenuButton = ({
   profile,
   path,
 }: MenuButtonProps) => {
+  const [open, setOpen] = useRecoilState(navState);
+  const setModal = useSetRecoilState(modalOpenState);
+
+  const navigate = useNavigate();
+
+  const handleNavOpen = () => {
+    if (id === 'search' || id === 'alarm') {
+      setOpen(true);
+      setModal(true);
+    } else if (id === 'direct') {
+      setOpen(true);
+      setModal(false);
+    } else {
+      setOpen(false);
+      setModal(false);
+    }
+  };
   return (
     <div
       className={`hover:bg-gray-100 rounded-lg overflow-hidden cursor-pointer
@@ -33,7 +53,13 @@ const MenuButton = ({
       ${borderFormat && selected ? 'border border-neutral-200' : ''}
       ${borderFormat && selected ? 'bg-transparent' : ''}
       `}
-      onClick={() => onClick(id)}
+      onClick={() => {
+        onClick(id);
+        handleNavOpen();
+        if (path) {
+          navigate(`${path}`);
+        }
+      }}
     >
       <div
         className={`
@@ -58,7 +84,7 @@ const MenuButton = ({
           />
         )}
       </div>
-      <p className='hidden xl:block'>{title}</p>
+      <p className={`${open ? '' : 'xl:block'} hidden`}>{title}</p>
     </div>
   );
 };
