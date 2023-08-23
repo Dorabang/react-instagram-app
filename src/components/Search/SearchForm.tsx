61ba9b57fb327bcc53f1897281489291b9e66632
Dorabang <psa1195@naver.com>
2023-08-24 01:22:10 +0900

    feat: 검색창 컴포넌트,(모바일 헤더 부분에 따로 들어가기 때문에 컴포넌트 분리)

---
+++ src/components/Search/SearchForm.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Input from '../Input';
+import { useRecoilState } from 'recoil';
+import { inputState } from '../../recoil/atoms';
+
+interface SearchFormProps {
+  addSearchValue: (value: string) => void;
+}
+
+const SearchForm = ({ addSearchValue }: SearchFormProps) => {
+  const [search, setSearch] = useRecoilState(inputState);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (search !== '') {
+      addSearchValue(search);
+      setSearch('');
+    }
+    return;
+  };
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <Input placeholder='검색' />
+    </form>
+  );
+};
+
+export default SearchForm;
