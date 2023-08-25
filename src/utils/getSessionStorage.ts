4a288d435be168b747019b46af67203ad3ef261e
Dorabang <psa1195@naver.com>
2023-08-25 19:37:45 +0900

    feat: getSession에 key 값 추가

--- src/utils/getSessionStorage.ts
+++ src/utils/getSessionStorage.ts
@@ -1,11 +1,14 @@
+import { UserInfoProps } from '../components/Auth/AuthForm';
+
 function getItem(key: string) {
   const value = sessionStorage.getItem(key);
 
-  if (key === 'searchList') return value === null ? null : JSON.parse(value);
+  if (key === 'searchList' || key === 'user')
+    return value === null ? null : JSON.parse(value);
   else return value === null ? [] : JSON.parse(value);
 }
 
-function setItem(key: string, value: string[]) {
+function setItem(key: string, value: string[] | UserInfoProps[]) {
   if (value === null || value === undefined) return;
 
   const toJson = JSON.stringify(value);
