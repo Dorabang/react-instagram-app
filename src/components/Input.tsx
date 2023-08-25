16c0230e6557e22caa1d8a5bcb2e43e0ac4b0242
Dorabang <psa1195@naver.com>
2023-08-25 19:36:05 +0900

    feat: search 검색 인풋

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,17 +5,28 @@ import { useRecoilState } from 'recoil';
 
 interface InputProps {
   type?: string;
+  required?: boolean;
+  pattern?: string;
   disabled?: boolean;
   placeholder?: string;
+  name?: string;
 }
 
-const Input = ({ type = 'text', disabled, placeholder = '' }: InputProps) => {
+const Input = ({
+  type = 'text',
+  required = false,
+  pattern,
+  disabled,
+  placeholder = '',
+  name,
+}: InputProps) => {
   const [value, setValue] = useRecoilState(inputState);
 
   return (
     <span className='relative'>
       <input
         type={type}
+        required={required}
         disabled={disabled}
         value={value}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,21 +40,40 @@ const Input = ({ type = 'text', disabled, placeholder = '' }: InputProps) => {
       w-full
       rounded-lg
       outline-none
-      ${type === 'text' ? 'bg-neutral-100' : ''}
+      ${name === 'search' ? 'bg-neutral-100' : ''}
       `}
       />
-      <div
-        className='
+      {name === 'search' && (
+        <div
+          className='
         absolute right-2 top-1/2 -translate-y-1/2 w-[14px] h-[14px]
         flex justify-center items-center
         text-white bg-gray-300
         rounded-full leading-none
         cursor-pointer
         '
-        onClick={() => setValue('')}
-      >
-        <IoClose size={12} className='text-gray-50' />
-      </div>
+          onClick={() => setValue('')}
+        >
+          <IoClose size={12} className='text-gray-50' />
+        </div>
+      )}
+      {(name === 'email' ||
+        name === 'password' ||
+        name === 'userName' ||
+        name === 'nickName') && (
+        <div
+          className='
+        absolute right-2 top-1/2 -translate-y-1/2 w-[14px] h-[14px]
+        flex justify-center items-center
+        text-white bg-gray-300
+        rounded-full leading-none
+        cursor-pointer
+        '
+          onClick={() => setValue('')}
+        >
+          <IoClose size={12} className='text-gray-50' />
+        </div>
+      )}
     </span>
   );
 };
