27b80efefe9f1e8e5e3b7ff59b257f69023d4dc2
Dorabang <psa1195@naver.com>
2023-08-25 19:32:39 +0900

    feat: 세션에 유저 정보가 없을 시, login 페이지로 리다이렉트

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { getItem } from './utils/getSessionStorage';
+
+export interface UserProps {
+  email: string;
+  password: string;
+}
 
 function App() {
+  const user: UserProps[] | undefined = getItem('userInfo');
+  console.log('user', user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    user?.length === 0 && navigate('/login');
+  }, [user]);
+
   return (
     <>
-      <Header />
+      {user?.length !== 0 && <Header />}
+      <Outlet />
     </>
   );
 }
