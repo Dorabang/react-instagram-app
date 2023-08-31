f9f7f0bcd962b7d8b190af58d282531a58e1fb5a
Dorabang <psa1195@naver.com>
2023-09-01 00:28:00 +0900

    design: 인스타 기능 설명 이미지 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,17 +3,21 @@ import AuthForm from '../../components/Auth/AuthForm';
 import { getItem } from '../../utils/getSessionStorage';
 import { UserProps } from '../../App';
 import { useNavigate } from 'react-router-dom';
+import loginImg from 'assets/instagram_login.png';
 
 const LoginPage = () => {
-  const user: UserProps[] | undefined = getItem('userInfo');
-
-  const navigate = useNavigate();
-  /* 
-  useEffect(() => {
-    user?.length !== 0 && navigate('/login' || '/register');
-  }, [user]); */
-
-  return <AuthForm type='login' />;
+  return (
+    <div className='flex justify-center items-center'>
+      <div className='w-full max-w-[850px] flex justify-center items-center flex-grow'>
+        <img
+          src={loginImg}
+          alt='인스타그램 기능 설명 이미지'
+          className='mr-6'
+        />
+        <AuthForm type='login' />
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
