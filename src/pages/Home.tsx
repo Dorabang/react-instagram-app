5e7fed2edf97e880e4eb8f3b385903251c636888
Dorabang <psa1195@naver.com>
2023-09-03 20:26:29 +0900

    feat: 홈 화면에 게시물 모달 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PostCard from 'components/PostCard';
 import RecommendUser from 'components/RecommendUser';
+import PostModal from 'components/PostModal';
+import { useRecoilValue } from 'recoil';
+import { postModalState, postState } from 'recoil/atoms';
 
 const Home = () => {
+  const modalId = useRecoilValue(postModalState);
+  const posts = useRecoilValue(postState);
+  const post = posts.find((post) => post.id === modalId);
+
   return (
     <>
       <PostCard />
-      <div className='hidden xl:block'>
+      {modalId !== null ? <PostModal id={modalId} post={post} /> : null}
+      <div className='hidden xl:block pl-16'>
         <RecommendUser />
       </div>
     </>
