3ba13eb52402c17fc40cf9b536da90dcd98910f0
Dorabang <psa1195@naver.com>
2023-09-03 20:22:35 +0900

    chore: 게시물 모달창 컴포넌트 외부 클릭 시 닫기 추가

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,16 +1,19 @@
 import { useSetRecoilState } from 'recoil';
 import useOnClickOutside from 'hooks/useOnClickOutside';
 import React, { useRef } from 'react';
-import { modalOpenState, navState } from 'recoil/atoms';
+import { modalOpenState, navState, postModalState } from 'recoil/atoms';
 
 const Modal = ({ children }: { children: React.ReactNode }) => {
   const ref = useRef<HTMLDivElement>(null);
   const setOpen = useSetRecoilState(navState);
 
   const setModal = useSetRecoilState(modalOpenState);
+  const setPostModal = useSetRecoilState(postModalState);
+
   useOnClickOutside(ref, () => {
     setModal(false);
     setOpen(false);
+    setPostModal(null);
   });
   return <div ref={ref}>{children}</div>;
 };
